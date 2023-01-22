b27226f213f3c3363e8e69b14cfb706aaa5ed66a
Sebastian <sebastian.proost@gmail.com>
2023-01-22 21:37:58 +0100

    testing better solution

--- _plugins/image_size.rb
+++ _plugins/image_size.rb
@@ -4,7 +4,7 @@ Jekyll::Hooks.register :documents, :pre_render, priority: 1 do |page|
     page.content = page.content.gsub(/\!\[(.*?)\]\({{ site.baseurl }}(.*?)\)/) do |match|
         # puts("#{Jekyll.sites.first.source}#{$2}")
         width, height = FastImage.size("#{Jekyll.sites.first.source}#{$2}")
-        output = "![#{$1}]({{ site.baseurl }}#{$2}){: style=\"aspect-ratio: #{width}/#{height}\"}"
+        output = "![#{$1}]({{ site.baseurl }}#{$2}){: height=\"#{height}\" width=\"#{width}\"}"
         # puts(output)
         output
     end
