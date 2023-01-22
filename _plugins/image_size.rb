4a7c97371c8f507fe8fbe953a141ca5f49db5f9a
Sebastian <sebastian.proost@gmail.com>
2023-01-22 21:26:23 +0100

    fixed typo

--- _plugins/image_size.rb
+++ _plugins/image_size.rb
@@ -2,11 +2,10 @@ require 'fastimage'
 
 Jekyll::Hooks.register :documents, :pre_render, priority: 1 do |page|
     page.content = page.content.gsub(/\!\[(.*?)\]\({{ site.baseurl }}(.*?)\)/) do |match|
-        puts("#{Jekyll.sites.first.source}#{$2}")
+        # puts("#{Jekyll.sites.first.source}#{$2}")
         width, height = FastImage.size("#{Jekyll.sites.first.source}#{$2}")
-        # output = "![#{$1}]({{ site.baseurl }}#{$2}){: height=\"#{height}\" width=\"#{width}\"}"
-        output = "![#{$1}]({{ site.baseurl }}#{$2}){: style=\"aspect-ratio #{width}/#{height}\"}"
-        puts(output)
+        output = "![#{$1}]({{ site.baseurl }}#{$2}){: style=\"aspect-ratio: #{width}/#{height}\"}"
+        # puts(output)
         output
     end
 end
